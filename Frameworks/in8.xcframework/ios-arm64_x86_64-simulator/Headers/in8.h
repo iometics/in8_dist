4fc818de6eab68e00344bb3776341f45c07c1761
Guy Stewart <stewart.guy@gmail.com>
2026-01-31 08:46:13 -0800

    initial commit

---
+++ Frameworks/in8.xcframework/ios-arm64_x86_64-simulator/Headers/in8.h
@@ -0,0 +1,12 @@
+// in8.h - Main header for in8 framework
+#ifndef IN8_H
+#define IN8_H
+
+// Include your public API headers here
+// For example:
+// #include "in8/platform.h"
+// #include "in8/engine.h"
+
+// This is a placeholder - customize based on your actual public API
+
+#endif // IN8_H
